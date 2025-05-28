fac76265774aec78ef75a6f3b0fc0152f58dbf13
rajvi <50464351+rmai21@users.noreply.github.com>
2025-05-27 23:30:47 -0400

    set-up flask app

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+# Setting up flask app    
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+        return "This is my backend!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
